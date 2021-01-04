372f21465bafa7f7055a4ad1b7c513d11ec86ee9
Valeria Copleman <valeriacopleman@gmail.com>
2021-01-03 16:26:05 -0800

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 class Formatter {
   //add static methods here
+  static capitalize(str){
+    return str.charAt(0).toUpperCase() + str.slice(1)
+  }
+  static sanitize(str) {
+    return str.replace( /[^A-Za-z0-9 '-]/g, '' )
+  }
+  static titleize(str) {
+    const smalls = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    return str.split(" ").map((word, i)=> smalls.includes(word) && i !== 0 ? word : word.charAt(0).toUpperCase() + word.slice(1)).join(" ")
+  }
 }
